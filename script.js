21848a1e5707885af7884c15187ffc9fa55530b5
Edison <jedisoncortes@gmail.com>
2025-03-04 20:33:36 -0500

    Actualizacion 2

--- script.js
+++ script.js
@@ -1,51 +1,17 @@
 document.querySelector('h1').addEventListener('click',()=> {
     document.body.style.background = 'violet';
 })
-// document.getElementById('image').addEventListener('click',()=> {
-    // document.body.style.background = 'violet';
-
-var imagenes = ['imagenes/perro1.png,imagenes/pg.png,imagenes/productos2.png,imagenes/productos3.png'],
-    cont = 0;
-
-function carrusel(contenedor){
-    contenedor.addEventListener('click', e => {
-        let atras = contenedor.querySelector('.atras botones'),
-            adelante = contenedor.querySelector('adelante botones'),
-            imagenes = contenedor.querySelector('imagenes'),
-            tgt = e.target;
-
-        if(tgt == atras) {
-            if(cont >0){
-                img.src = imagenes[cont - 1];
-                cont --;
-            } else {
-                img.src = imagenes[imagenes.length - 1];
-                cont = imagenes.length - 1;
-            } 
-
-        } else if(tgt == adelante){
-            if(cont < imagenes.length - 1){
-                img.src = imagenes[cont + 1];
-                cont ++;
-            } else {
-                img.src = imagenes[0];
-                cont = 0;
-            } 
-    }
-
-});
-
-}
-
-document.addEventListener("DOMcontentLoaded",() => {
-    let contenedor = document.querySelector('contenedor');
-
-    carrusel(contenedor);
-
-}
-);
 
 function subscribe() {
     const email = document.getElementById('email').value;
     alert('Suscripción correcta con el email: ' + email)
+}
+
+function mover(direccion) {
+    const carrusel = document.querySelector('.carrusel');
+    const imagenes = document.querySelectorAll('.carrusel img');
+    index += direccion;
+    if (index < 0) index = imagenes.length - 1;
+    if (index >= imagenes.length) index = 0;
+    carrusel.style.transform = `translateX(-${index * 400}px)`;
 }
